a00364d1ad7b7bbe8e9d533ac58010cb2a5a922b
namratam <namrataucg@gmail.com>
2022-04-23 23:38:22 -0700

    Updated job submitter with maappy

--- cwl_generic_steps/job_submitter.py
+++ cwl_generic_steps/job_submitter.py
@@ -1,14 +1,17 @@
-import maapypy
+from maap.maap import MAAP
 import requests
 import os
+import json
 from jinja2 import Environment, FileSystemLoader
 import logging
+
 
 log_format = "[%(asctime)s: %(levelname)s/%(name)s/%(funcName)s] %(message)s"
 logging.basicConfig(format=log_format, level=logging.INFO)
 logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
 
-api_dps_url = "https://api.imgspec.org/api/dps/job"
+api_dps_url = "https://api.imgspec.org/api/dps/job" # TODO: Move this to a config file
+maap = MAAP(not_self_signed=False)
 
 
 def norm_path(path):
@@ -16,46 +19,60 @@ def norm_path(path):
     return os.path.abspath(os.path.normpath(path))
 
 
-def create_execute_payload(execute_info):
-    # Read in jinja template
-    tmpl_file_dir = norm_path(os.path.join(os.path.dirname(__file__), "..", "api_interface_templates"))
-    file_loader = FileSystemLoader(tmpl_file_dir)
-    env = Environment(loader=file_loader)
-    template = env.get_template("wps_execute_request.jinja2.tmpl")
-    request = template.render(data=execute_info)
-    return request
-
-
-def parse_execute_response(resp):
-    return
-
-def wps_execute(job_info):
-    job_id = None
+def job_execute(job_info):
+    """
+    Submit a PGE job and return job id
+    :param job_info:
+    :return:
+    """
     try:
-        # TODO: change this to maappy
-        headers = {"Content-type: application/xml"}
-        payload = create_execute_payload(execute_info=job_info)
-        response = requests.request("POST", api_dps_url, headers=headers, data=payload)
+        # get details of job
+        algo = job_info.get("job_type").split(":")
+        algo_id = algo[0]
+        algo_version = algo[1]
+        inputs = job_info.get("inputs")
+        logger.info("Submitting job {} of version {} \nto queue {} \nwith inputs \n {}".format(
+            algo_id, algo_version, job_info.get("resource"), json.dump(inputs)
+        ))
+        # submit job
+        job = maap.submitJob(
+            algo_id=algo_id,
+            version=algo_version,
+            queue=job_info.get("resource"),
+            **inputs
+        )
+        logging.info("Job submission response:\n{}".format(job))
+        return job
     except Exception as ex:
-        raise()
-    try:
-        # parse response and find job id
-        job_id = parse_execute_response(response)
-    except Exception as ex:
-        raise()
-    return job_id
+        # TODO: Update with chain of exceptions
+        raise("Caught Exception submitting job. {}".format(ex))
 
 
 def wps_poll_for_status(job_id):
     # TODO: Implement new maap py handler for polling and use it here
-    return
+    job_status = maap.waitForJobCompletion(job_id)
+    status = job_status.get("status")
+    return status
+
 
 def wps_get_result(job_id):
-    # TODO: Call maap py get result
+    """
+    Get the result of a job
+    :param job_id:
+    :return:
+    """
+    logger.info("Getting Job Result for job {}".format(job_id))
+    job_result = maap.getJobResult(job_id)
     # check if product generated
+    logger.info("Result:\n{}".format(job_result))
+    outputs = job_result.get("outputs")
     # if nothing then return None
-    # else return URL(s)
-    return
+    if len(outputs) > 0:
+        return outputs
+    else:
+        logger.info("No products generated from job")
+        return[]
+
 
 def main():
     # identify inputs - name of algorithm:version, inputs dictionary
@@ -73,15 +90,23 @@ def main():
         }
     """
     # call execute
-    job_id = wps_execute(job_info)
-    job_status = wps_poll_for_status(job_id)
-    if job_status == "successful":
+    job = job_execute(job_info)
+    job.waitForJobCompletion()
+    if job.status.lower() == "succeeded":
         # get job's result
-        result = wps_get_result(job_id)
-    elif job_status in ["failed", "dismissed"]:
-        raise()
+        job.getJobResult()
+        outputs = job.outputs
+        # if nothing then return None
+        if len(outputs) == 0:
+            logger.info("No products generated from job")
+    else:
+        if job.status.lower() == "failed":
+            raise RuntimeError("Job failed. Traceback:\n{}".format(job.traceback))
+        else:
+            raise RuntimeError("Job was not successfully completed. Status of job is {}.".format(job.status.lower()))
     # formulate output of cwl step
-    return output
+    result = dict()
+    return result
 
 
 if __name__ == "__main__":
