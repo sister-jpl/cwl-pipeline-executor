eaa448c3c96b6a0113c0fcca355fd6b268c26f70
Sujen Shah <sujen1412@gmail.com>
2022-04-27 18:04:41 -0700

    Add ENV for MAAP_CONF

--- build-env.sh
+++ build-env.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 mamba create -n cwl-pipeline -c conda-forge -y cwltool
+
+pushd /
+rm -rf maap-py
+git clone --single-branch system-test-8 https://gitlab.com/geospec/maap-py.git
+pushd maap-py
+pip install -e .
