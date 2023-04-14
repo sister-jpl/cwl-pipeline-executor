771b4e0d7f23e76acfae93977fee75ef64cea12f
Sujen Shah <sujen1412@gmail.com>
2023-04-14 15:20:48 -0700

    Allow users to choose workflow to run

--- run-cwl.sh
+++ run-cwl.sh
@@ -3,13 +3,14 @@
 # source activate cwl-pipeline
 set -ex
 
+WORKFLOW_NAME=$1
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 python ${basedir}/create_workflow_inputs.py inputs.json
 WORKFLOW_INPUTS=$(ls $PWD/workflow-inputs.yml)
 
 # pushd is important for the ymls to find the python file doing a $PWD
 pushd ${basedir}/workflow
-cwltool sister-workflow.yml ${WORKFLOW_INPUTS}
+cwltool ${WORKFLOW_NAME}.yml ${WORKFLOW_INPUTS}
 popd
 
 set -x
