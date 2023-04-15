4c601fbb581397f2488f9213f9f43b00c49cfe3c
Sujen Shah <sujen1412@gmail.com>
2023-04-14 17:45:58 -0700

    Install --upgrade importlib

--- build-env.sh
+++ build-env.sh
@@ -2,6 +2,8 @@
 set -x
 mamba install -c conda-forge -y cwltool
 pushd /app
+# To mitigate AttributeError: module 'importlib_metadata' has no attribute 'MetadataPathFinder'
+pip install --upgrade importlib-metadata
 git clone --single-branch --branch sister-dev https://gitlab.com/geospec/maap-py.git
 pushd maap-py
 pip install -e .
