f0db78c8bfe13e2fe3066eb49c68b75f706b909e
Sujen Shah <sujen1412@gmail.com>
2023-04-14 17:17:24 -0700

    Set debug mode for build script

--- build-env.sh
+++ build-env.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+set -x
 mamba install -c conda-forge -y cwltool
 pushd /app
-pip install pyopenssl --upgrade
 git clone --single-branch --branch sister-dev https://gitlab.com/geospec/maap-py.git
 pushd maap-py
 pip install -e .
