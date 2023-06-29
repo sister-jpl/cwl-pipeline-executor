7ac073ef0f300baa64b067fc972043fd5e00ccdc
Sujen <sujen1412@gmail.com>
2023-06-29 21:30:40 +0000

    Update build-env.sh to download and unzip coral mask file

--- build-env.sh
+++ build-env.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -x
-mamba create -n cwl-pipeline -c conda-forge -y cwltool nodejs
+mamba create -n cwl-pipeline -c conda-forge -y cwltool nodejs awscli
 source activate cwl-pipeline
 pushd /app
+# Download Coral Mask file
+aws s3 cp s3://sister-ops-registry/packages/Coralshapefile20230526123457.zip .
+unzip Coralshapefile20230526123457.zip
+
 # To mitigate AttributeError: module 'importlib_metadata' has no attribute 'MetadataPathFinder'
 pip install --upgrade importlib-metadata
 git clone --single-branch --branch sister-dev https://gitlab.com/geospec/maap-py.git
