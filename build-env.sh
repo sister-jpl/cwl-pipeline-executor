71d7542ec9c4dfe0cec115f4de04bdacf4d1364b
Sujen <sujen1412@gmail.com>
2023-07-11 21:03:19 +0000

    Update build-env.sh to install geopandas

--- build-env.sh
+++ build-env.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 set -x
-mamba create -n cwl-pipeline -c conda-forge -y cwltool nodejs awscli
-source activate cwl-pipeline
-pushd /app
-# Download Coral Mask file
-aws s3 cp s3://sister-ops-registry/packages/Coralshapefile20230526123457.zip .
-unzip Coralshapefile20230526123457.zip
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
 
+mamba create -n cwl-pipeline -c conda-forge -y cwltool nodejs awscli geopandas=0.12.2
+source activate cwl-pipeline
+
+# Download Coral Mask file
+aws s3 cp s3://sister-ops-registry/packages/Coralshapefile20230526123457.zip ${basedir}/workflow/preconditions/Coralshapefile20230526123457.zip
+unzip ${basedir}/workflow/preconditions/Coralshapefile20230526123457.zip
+
+pushd /app
 # To mitigate AttributeError: module 'importlib_metadata' has no attribute 'MetadataPathFinder'
 pip install --upgrade importlib-metadata
 git clone --single-branch --branch sister-dev https://gitlab.com/geospec/maap-py.git
